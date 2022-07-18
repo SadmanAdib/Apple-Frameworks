9a851a0e77cfbe5e7e17969836b9afb33ab34f24
Sadman Adib <sadmanadib67@gmail.com>
2022-07-18 17:18:17 +0600

    cross added

--- Apple-Frameworks/DetailView.swift
+++ Apple-Frameworks/DetailView.swift
@@ -13,6 +13,20 @@ struct DetailView: View {
     
     var body: some View {
         VStack{
+            
+            HStack{
+                Spacer()
+                Button{
+                    print("cross pressed")
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .padding()
+            
             Spacer()
             
             FrameworkTitleView(framework: framework)
