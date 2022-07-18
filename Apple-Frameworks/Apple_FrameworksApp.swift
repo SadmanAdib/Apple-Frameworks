f9d2d9413665ebc1d6323eed92e1d7dbefc357e6
Sadman Adib <sadmanadib67@gmail.com>
2022-07-18 13:32:16 +0600

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Sadman Adib on 18/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
