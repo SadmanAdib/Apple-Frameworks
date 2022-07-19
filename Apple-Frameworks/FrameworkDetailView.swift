b6900b9acc3c2fc6e3d02a66afac59b85a20db4a
Sadman Adib <sadmanadib67@gmail.com>
2022-07-19 13:06:23 +0600

    Safari view shown upon pressing button

--- Apple-Frameworks/FrameworkDetailView.swift
+++ Apple-Frameworks/FrameworkDetailView.swift
@@ -11,6 +11,7 @@ struct FrameworkDetailView: View {
     
     var framework: Framework
     @Binding var isShowingDetailView: Bool
+    @State private var isShowingSafariView = false
     
     var body: some View {
         VStack{
@@ -38,9 +39,17 @@ struct FrameworkDetailView: View {
             
             Spacer()
             
-            AFButton(title: "Learn more")
+            Button {
+                isShowingSafariView = true
+            } label: {
+                AFButton(title: "Learn more")
+                
+            }
            
         }
+        .sheet(isPresented: $isShowingSafariView, content: {
+            SafariView(url: URL(string: framework.urlString) ?? URL(string: "www.apple.com")!)
+        })
     }
 }
 
@@ -49,5 +58,7 @@ struct FrameworkDetailView_Previews: PreviewProvider {
         FrameworkDetailView(framework: MockData.sampleFramework, isShowingDetailView: .constant(false))
     }
 }
+
+
 
 
