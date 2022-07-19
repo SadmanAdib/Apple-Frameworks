b8e7dda05de0f61e375adb506ff08cd9613fc473
Sadman Adib <sadmanadib67@gmail.com>
2022-07-19 14:18:24 +0600

    Refactored

--- Apple-Frameworks/FrameworkGridViewModel.swift
+++ Apple-Frameworks/FrameworkGridViewModel.swift
@@ -5,16 +5,16 @@
 //  Created by Sadman Adib on 19/7/22.
 //
 
-import Foundation
+import SwiftUI
 
 final class FrameworkGridViewModel: ObservableObject {
     
-    var selectedFramework: Framework?{
-        didSet{
-            isShowingDetailView = true
-        }
+    var selectedFramework: Framework? {
+        didSet{ isShowingDetailView = true }
     }
     
     @Published var isShowingDetailView = false
+    
+    let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
     
 }
---
+++ Apple-Frameworks/FrameworkTitleView.swift
@@ -0,0 +1,33 @@
+//
+//  FrameworkTitleView.swift
+//  Apple-Frameworks
+//
+//  Created by Sadman Adib on 19/7/22.
+//
+
+import SwiftUI
+
+struct FrameworkTitleView: View {
+    
+    var framework: Framework
+    
+    var body: some View{
+        VStack{
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
+}
+
+struct FrameworkTitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkTitleView(framework: MockData.sampleFramework)
+    }
+}
