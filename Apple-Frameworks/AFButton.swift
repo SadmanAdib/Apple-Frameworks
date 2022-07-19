fd2f39cb9dabed2c9969756451b45079995c5989
Sadman Adib <sadmanadib67@gmail.com>
2022-07-19 11:57:22 +0600

    Button view extracted

---
+++ Apple-Frameworks/AFButton.swift
@@ -0,0 +1,34 @@
+//
+//  AFButton.swift
+//  Apple-Frameworks
+//
+//  Created by Sadman Adib on 18/7/22.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    
+    var title: String
+    
+    var body: some View {
+        Button {
+            print("button pressed")
+        } label: {
+            Text(title)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .frame(width: 280, height: 50)
+                .background(.red)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            
+        }
+    }
+}
+
+struct AFButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AFButton(title: "Test title")
+    }
+}
